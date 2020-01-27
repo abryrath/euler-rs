e8a834e589693d7e261a6ce5c8907051a6dd87a6
Abry Rath <abry.rath@union.co>
2020-01-27 07:45:53 -0500

    p002 solution

--- src/bin/p002.rs
+++ src/bin/p002.rs
@@ -1,25 +1,28 @@
+#[macro_use(problem)]
+extern crate common;
 extern crate iterators;
 
 use iterators::fib::Fib;
 
-// fn main() {
-//     let mut f = Fib::<u64>::new();
-//     // for n in f.take(10) {
-//     //     let i = 
-//     //     println!("{}", i);
-//     // }
-//     loop {
-//         let o = f.next();
-//         match o {
-//             Some(n) => {
-//                 println!("{}: {}", &f.i, n);
-//                 if &f.i > &10 {
-//                     break;
-//                 }
-//             },
-//             None => println!("wtf"),
-//         }
-//         // println!("{}: {}", i, n);
+fn compute(bound: u32) -> u32 {
+    Fib::<u32>::new()
+        .take_while(|&n| n < bound)
+        .filter(|&n| n % 2 == 0)
+        .sum()
+}
 
-//     }
-// }
+fn solve() -> String {
+    compute(4_000_000u32).to_string()
+}
+
+problem!("4613732", solve);
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn sum_first_ten_terms() {
+        let terms = [2, 8, 34];
+        let sum: u32 = terms.iter().sum();
+        assert_eq!(sum, super::compute(100));
+    }
+}
